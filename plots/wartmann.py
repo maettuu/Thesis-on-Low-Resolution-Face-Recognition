0b0d39fb6b11592c2c7262b1a996640e6233d108
maettuu <mangori41@bluewin.ch>
2022-08-19 15:24:45 +0200

    update wartmann.py

--- plots/wartmann.py
+++ plots/wartmann.py
@@ -17,7 +17,7 @@ from bokeh.models import ColumnDataSource
 
 # reads data from files
 def get_data(cohort_size, parameter):
-    filename = "wartmann-" + cohort_size + parameter + ".csv"
+    filename = "wartmann-" + cohort_size + "-" + parameter + ".csv"
     # skip first two rows
     df = pd.read_csv(filename, skiprows=[i for i in range(1, 3)])
     # only read every 3rd row
@@ -75,10 +75,10 @@ def configure_plot(plot, protocol):
 
 # used to generate plot
 def generate_plot(cohort_size, protocol):
-    _, source_alpha = get_data(cohort_size, "-alpha")
-    _, source_beta = get_data(cohort_size, "-beta")
-    _, source_both = get_data(cohort_size, "-both")
-    df_quads, source_quads = get_data(cohort_size, "")
+    _, source_alpha = get_data(cohort_size, "alpha")
+    _, source_beta = get_data(cohort_size, "beta")
+    _, source_both = get_data(cohort_size, "both")
+    df_various, _ = get_data(cohort_size, "various")
 
     plot = get_figure()
 
@@ -88,14 +88,14 @@ def generate_plot(cohort_size, protocol):
     plot.line(source=source_beta, **beta_args)
     plot.line(source=source_both, **both_args)
 
-    plot.quad(legend_label="1.25 ≤ beta ≤ 3.0", **get_quad_args(1.24, 1.26, df_quads, protocol, 0, 7))
-    plot.quad(**get_quad_args(1.49, 1.51, df_quads, protocol, 7, 14))
-    plot.quad(**get_quad_args(1.74, 1.76, df_quads, protocol, 14, 21))
-    plot.quad(**get_quad_args(1.99, 2.01, df_quads, protocol, 21, 28))
-    plot.quad(**get_quad_args(2.24, 2.26, df_quads, protocol, 28, 35))
-    plot.quad(**get_quad_args(2.49, 2.51, df_quads, protocol, 35, 42))
-    plot.quad(**get_quad_args(2.74, 2.76, df_quads, protocol, 42, 49))
-    plot.quad(**get_quad_args(2.99, 3.01, df_quads, protocol, 49, 56))
+    plot.quad(legend_label="1.25 ≤ beta ≤ 3.0", **get_quad_args(1.24, 1.26, df_various, protocol, 0, 7))
+    plot.quad(**get_quad_args(1.49, 1.51, df_various, protocol, 7, 14))
+    plot.quad(**get_quad_args(1.74, 1.76, df_various, protocol, 14, 21))
+    plot.quad(**get_quad_args(1.99, 2.01, df_various, protocol, 21, 28))
+    plot.quad(**get_quad_args(2.24, 2.26, df_various, protocol, 28, 35))
+    plot.quad(**get_quad_args(2.49, 2.51, df_various, protocol, 35, 42))
+    plot.quad(**get_quad_args(2.74, 2.76, df_various, protocol, 42, 49))
+    plot.quad(**get_quad_args(2.99, 3.01, df_various, protocol, 49, 56))
 
     configure_plot(plot, protocol)
 
